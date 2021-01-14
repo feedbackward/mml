194f4850a58290ab4d2cb264473810bf4d7bcc6e
mjh <matthew-h@ar.sanken.osaka-u.ac.jp>
2021-01-14 17:24:38 +0900

    Added multi-output linreg object.

--- mml/models/linreg.py
+++ mml/models/linreg.py
@@ -35,6 +35,35 @@ class LinearRegression(Model):
     def hess(self, w=None, X=None):
         n, d = X.shape
         return np.zeros((d,d,n), dtype=w.dtype)
-    
 
+
+class LinearRegression_Multi(Model):
+    '''
+    Linear regression model (multiple outputs).
+    '''
+    
+    def __init__(self, w_init, name="Multi-output linear regression"):
+        super(LinearRegression_Multi, self).__init__(w_init=w_init,
+                                                     name=name)
+        return None
+    
+    
+    def func(self, w=None, X=None):
+        if w is None:
+            return np.matmul(X,self._w)
+        else:
+            return np.matmul(X,w)
+    
+    
+    def grad(self, w=None, X=None):
+        '''
+        Since this is multi-output, the Jacobian.
+        '''
+        num_classes = self._w.shape[1] if w is None else w.shape[1]
+        return np.broadcast_to(
+            array=np.expand_dims(X, axis=len(X.shape)),
+            shape=X.shape+(num_classes,)
+        )
+    
+    
 ###############################################################################
